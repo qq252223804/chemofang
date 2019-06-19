dcc9c058d5b14dbd9ffa66f464f78595cc4f1228
taojian <252223804@qq.com>
2019-06-19 10:16:06 +0800

    更改sql

--- Common/SQL_execute.py
+++ Common/SQL_execute.py
@@ -3,13 +3,18 @@
 # @Author : taojian 
 # @File : SQL_execute.py
 # 封装mysql  mysql_info 连接池配置信息
+
+#连接数据库
+#测试数据库
+# config={
+#     "host":"192.168.3.216",
+#     "user":"root",
+#     "password":"66ifuel",
+#     "database":"charging",
+#     "charset": "utf8"}
+#外网测试数据库
 import pymysql
-
-from utx import Log
-
-
-import pymysql
-from utx import *
+from Common.log import Log
 
 #连接数据库 port必须为int %d类型
 config={
@@ -31,10 +36,9 @@ def mysql_execute(sql, number=None):
     # with db.cursor(cursor=pymysql.cursors.DictCursor) as cursor:#获取数据库连接的对象以字典形式
     with db.cursor() as cursor:
         try:
-
-
             if number == 'one':
                 cursor.execute(sql)
+                print('执行成功')
             elif number == 'more':
                 cursor.executemany(sql)
             else:
@@ -51,20 +55,23 @@ def mysql_getrows(sql, number=None):
     ''' 返回查询结果'''
     with db.cursor() as cursor:
         try:
-            cursor.execute(sql)
+            if number == 'one':
+                cursor.execute(sql)
+                # print('执行成功')
+                row = cursor.fetchone()
+                return row
+            elif number == 'more':
+                cursor.executemany(sql)
+                rows =cursor.fetchall()
+                return rows
+            else:
+                pass
         except Exception as a:
+            print("查询结果错误：%s" % a)
             Log().debug("查询结果错误：%s" % a)
-        # print("查询结果错误：%s" % a)
-        if number == 'one':
-            row = cursor.fetchone()
-            return row
-        elif number == 'more':
-            rows =cursor.fetchall()
-            return rows
         else:
-            pass
-        cursor.close()
-        db.close()
+            cursor.close()
+            db.close()
 def mysql_getstring(sql):
     '''查询一行的所有值'''
     rows = mysql_getrows(sql, 'one')
@@ -75,20 +82,35 @@ def mysql_getstring(sql):
         # for i in row:
         # 	print(i)
 
-sql = "select validCode FROM cp_messagecode WHERE phone='18657738815' order by id DESC;"
-print(mysql_getrows(sql, number='one')[0])
+# sql="select validCode FROM cp_messagecode WHERE phone='13279612508';"
+# code=mysql_getrows(sql, number='one')[0]
+# print(code)
 
 
-# sql = "select validCode FROM cp_messagecode WHERE phone=15027863095"
+
 # sql1 = "select * from order_info where order_no='20201812191425320005'"
-# sqls="UPDATE order_pledge SET status = 'CLOSED_DEALER' where user_id= '5b42f9b7cec76d75f185d4ec'"
-# sql2="select * from dealer_car where car_info_id= '5a5ec113943d0b515a43b33a' ORDER BY id DESC "
-# mysql.mysql_execute(sql1,number='one')
-# mysql.mysql_execute(sqls,number='one')
-# mysql.mysql_execute(sql2,number='one')
-# # print(mysql.mysql_getrows(sql1,'one'))
-# # print(mysql.mysql_getrows(sqls,'one'))
-# print(mysql.mysql_getrows(sql,number='one'))
+# sqls = "UPDATE order_pledge SET status = 'CLOSED_DEALER' where user_id= '5b42f9b7cec76d75f185d4ec'"
+# sql2 = "select * from dealer_car where car_info_id= '5a5ec113943d0b515a43b33a' ORDER BY id DESC "
+# mysql.mysql_execute(sql1, number='one')
+# mysql.mysql_execute(sqls, number='one')
+# mysql.mysql_execute(sql2, number='one')
+# print(mysql.mysql_getrows(
 #
-# # print(mysql.mysql_getstring(sql2))
-# mysql.mysql_close()
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+
+# sql1,'one'))
+# print(mysql.mysql_getrows(sqls,'one'))
+
+# print(mysql.mysql_getstring(sql))
+
